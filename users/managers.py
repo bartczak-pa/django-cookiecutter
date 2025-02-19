edffc2e03043dde7dffc2730e7150e74c0824e99
Pawel <pawel@bartczak.tech>
2025-02-19 16:20:25 +0000

    Modified Custom User Manager to handle email normalization

--- users/managers.py
+++ users/managers.py
@@ -1,43 +1,31 @@
 from typing import Any
 from django.contrib.auth.models import BaseUserManager
-from django.db.models import QuerySet
 
 
 class CustomUserManager(BaseUserManager):
     """
-    Custom manager for CustomUser model where email is the unique identifier
+    Custom manager for the CustomUser model where email is the unique identifier
     for authentication instead of usernames.
     """
 
     def create_user(self, email: str, password: str | None = None, **extra_fields: Any) -> "CustomUser":
-        """
-        Create and return a regular user with the given email and password.
-        """
+        """Create and return a regular user with the given email and password."""
         if not email:
             raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        extra_fields.setdefault('is_active', True)
-        user = self.model(email=email, **extra_fields)  # type: ignore [attr-defined]
+        email = self.normalize_email(email).lower()  # Normalize the entire email
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email: str, password: str | None = None, **extra_fields: Any) -> "CustomUser":
-        """
-        Create and return a superuser with the given email and password.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        """Create and return a superuser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get('is_staff') is not True:
+        if not extra_fields["is_staff"]:
             raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get('is_superuser') is not True:
+        if not extra_fields["is_superuser"]:
             raise ValueError("Superuser must have is_superuser=True.")
 
         return self.create_user(email, password, **extra_fields)
-
-    def get_queryset(self) -> QuerySet:
-        """
-        Return the default queryset for this manager.
-        """
-        return super().get_queryset()
